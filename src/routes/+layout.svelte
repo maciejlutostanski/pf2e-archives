54ed8f5058d3b2bec0fc0832f9740c6d58bb03f8
Maciej Lutostański <maciej.lutostanski@baid.pl>
2023-01-22 13:22:52 +0100

    Init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Db from "$lib/Db.svelte";
+  import "../app.postcss";
+</script>
+
+<Db>
+  <main class="container">
+    <slot />
+  </main>
+</Db>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<h1>Test</h1>
